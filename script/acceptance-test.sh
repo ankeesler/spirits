c5c47555453b96beb534ec4a94efcdeb53c9bbee
Andrew Keesler <ankeesler1@gmail.com>
2022-03-07 21:11:47 -0500

    script: acceptance-test.sh no longer used

--- script/acceptance-test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -exuo pipefail
-
-REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-
-cd "$REPO_ROOT"
-SPIRITS_TEST_BASE_URL=https://oh-great-spirits.herokuapp.com go test -count 1 -v ./test
