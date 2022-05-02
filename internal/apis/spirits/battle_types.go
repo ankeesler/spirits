d15122826f00ece30feda47fc805d2d76a80ff29
Andrew Keesler <akeesler@vmware.com>
2022-05-02 11:46:24 -0400

    Remove stuff not related to generation
    Signed-off-by: Andrew Keesler <akeesler@vmware.com>

--- internal/apis/spirits/battle_types.go
+++ internal/apis/spirits/battle_types.go
@@ -14,33 +14,18 @@ const (
 	BattlePhaseError    BattlePhase = "Error"
 )
 
-// BattleSpec defines the desired state of Battle
 type BattleSpec struct {
-	// Spirits are the spirits involved in this Battle
 	Spirits []corev1.LocalObjectReference `json:"spirits"`
 }
 
-// BattleStatus defines the observed state of Battle
 type BattleStatus struct {
-	// Conditions represents the observations of a Battle's current state
-	// +patchMergeKey=type
-	// +patchStrategy=merge
-	// +listType=map
-	// +listMapKey=type
-	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
-
-	// Phase describes what stage of the battle lifecycle this Battle is in currently
-	Phase BattlePhase `json:"phase"`
-
-	Message string `json:"message"`
-
-	// InBattleSpirits holds the names of the Spirit's that are participating in this Battle
+	Conditions      []metav1.Condition            `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+	Phase           BattlePhase                   `json:"phase"`
+	Message         string                        `json:"message"`
 	InBattleSpirits []corev1.LocalObjectReference `json:"inBattleSpirits,omitempty"`
-
-	ActingSpirit corev1.LocalObjectReference `json:"actingSpirit,omitempty"`
+	ActingSpirit    corev1.LocalObjectReference   `json:"actingSpirit,omitempty"`
 }
 
-// Battle is the Schema for the battles API
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type Battle struct {
 	metav1.TypeMeta   `json:",inline"`
@@ -50,7 +35,6 @@ type Battle struct {
 	Status BattleStatus `json:"status,omitempty"`
 }
 
-// BattleList contains a list of Battle
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type BattleList struct {
 	metav1.TypeMeta `json:",inline"`
--- internal/apis/spirits/spirit_types.go
+++ internal/apis/spirits/spirit_types.go
@@ -7,86 +7,43 @@ import (
 type SpiritIntelligence string
 
 const (
-	// SpiritIntelligenceRoundRobin is the default intelligence for a Spirit.
-	// It describes a Spirit that performs actions in a sequential, deterministic order.
 	SpiritIntelligenceRoundRobin SpiritIntelligence = "RoundRobin"
-
-	// SpiritIntelligenceRandom describes a Spirit that performs actions in a random order.
-	SpiritIntelligenceRandom SpiritIntelligence = "Random"
-
-	// SpiritIntelligenceHuman describes a Spirit whose actions are driven by human interaction.
-	SpiritIntelligenceHuman SpiritIntelligence = "Human"
+	SpiritIntelligenceRandom     SpiritIntelligence = "Random"
+	SpiritIntelligenceHuman      SpiritIntelligence = "Human"
 )
 
 type SpiritPhase string
 
 const (
-	// SpiritPhasePending is the default phase for newly-created resources.
 	SpiritPhasePending SpiritPhase = "Pending"
-
-	// SpiritPhaseReady is the phase for a resource in a healthy state.
-	SpiritPhaseReady SpiritPhase = "Ready"
-
-	// SpiritPhaseError is the phase for a in an unhealthy state.
-	SpiritPhaseError SpiritPhase = "Error"
+	SpiritPhaseReady   SpiritPhase = "Ready"
+	SpiritPhaseError   SpiritPhase = "Error"
 )
 
-// SpiritStats are quantitative properties of the Spirit.
-// These are utilized and manipulated throughout the course of a Battle.
 type SpiritStats struct {
-	// Health is a quantitative representation of the energy of the Spirit.
-	// When this drops to 0, the Spirit is no longer able to participate in a Battle.
-	Health int `json:"health"`
-
-	// Power is a quantitative representation of the attacking ability of the Spirit.
-	// +optional
-	Power int `json:"power"`
-
-	// Armor is a quantitative representation of the defending ability of the Spirit.
-	// +optional
-	Armor int `json:"armor"`
-
-	// Agility is a quantitative representation of the speed of the Spirit.
-	// +optional
-	Agility int `json:"agility"`
+	Health  int
+	Power   int
+	Armor   int
+	Agility int
 }
 
 type SpiritSpecInternal struct {
 	Action Action
 }
 
-// SpiritSpec defines the desired state of Spirit
 type SpiritSpec struct {
-	// Stats are the current statistics that describe this Spirit
-	Stats SpiritStats `json:"stats"`
-
-	// Actions are the list of actions that this Spirit can perform
-	// +optional
-	Actions []string `json:"actions"`
-
-	// Intelligence describes how a Spirit will select actions to perform
-	// +optional
-	Intelligence SpiritIntelligence `json:"intelligence"`
-
-	Attributes map[string]string `json:"attributes,omitempty"`
-
-	Internal SpiritSpecInternal
+	Stats        SpiritStats
+	Actions      []string
+	Intelligence SpiritIntelligence
+	Attributes   map[string]string
+	Internal     SpiritSpecInternal
 }
 
-// SpiritStatus defines the observed state of Spirit
 type SpiritStatus struct {
-	// Conditions represents the observations of a Spirit's current state
-	// +patchMergeKey=type
-	// +patchStrategy=merge
-	// +listType=map
-	// +listMapKey=type
-	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
-
-	// Phase summarizes the overall status of the Spirit
-	Phase SpiritPhase `json:"phase,omitempty"`
+	Conditions []metav1.Condition
+	Phase      SpiritPhase
 }
 
-// Spirit is the Schema for the spirits API
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type Spirit struct {
 	metav1.TypeMeta   `json:",inline"`
@@ -96,7 +53,6 @@ type Spirit struct {
 	Status SpiritStatus `json:"status,omitempty"`
 }
 
-// SpiritList contains a list of Spirit
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type SpiritList struct {
 	metav1.TypeMeta `json:",inline"`
