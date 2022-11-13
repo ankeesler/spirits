21d081c135c6823ffa83363e79c85feaeb29bcef
Andrew Keesler <ankeesler1@gmail.com>
2022-11-12 22:03:55 -0500

    Ditch unused web app

--- pkg/api/generate.go
+++
@@ -1,3 +0,0 @@
-package api
-
-//go:generate protoc ../../api/action.proto ../../api/battle.proto ../../api/meta.proto ../../api/spirit.proto -I../../api --go_out=paths=source_relative:../../pkg/api --go-grpc_out=paths=source_relative:../../pkg/api
