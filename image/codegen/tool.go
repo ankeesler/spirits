2ae883a23a827dc00aa88b069b320a7fb2264675
Andrew Keesler <ankeesler@users.noreply.github.com>
2022-11-14 19:08:24 -0500

    Use custom Dockerfile instead of buf

---
+++ image/codegen/tool.go
@@ -0,0 +1,10 @@
+package tool
+
+import (
+	_ "github.com/envoyproxy/protoc-gen-validate"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "github.com/grpc-ecosystem/protoc-gen-grpc-gateway-ts"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
---
+++ script/codegen.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Run from root of repo
+cd "${MY_DIR}/.."
+
+docker run -v "${PWD}:/spirits" spirits-codegen \
+  --go_out=paths=source_relative:/spirits/pkg/api \
+  --go-grpc_out=paths=source_relative:/spirits/pkg/api \
+  --validate_out=lang=go,paths=source_relative:/spirits/pkg/api \
+  --grpc-gateway_out=paths=source_relative:/spirits/pkg/api/gateway \
+  -I/spirits/api \
+  /spirits/api/spirits/v1/meta.proto \
+  /spirits/api/spirits/v1/action.proto \
+  /spirits/api/spirits/v1/spirit.proto \
+  /spirits/api/spirits/v1/battle.proto
